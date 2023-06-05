063872012e3b34d7158498c729ea878909b2f386
Sanaullah <webdevfarsi@gmail.com>
2023-06-05 21:38:39 +0430

    React Profolio project

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+
+const Home = () => {
+  return (
+    <div className="h-screen relative top-0 left-0 bg-red-200 py-16">
+        {/* <h1>Home Page </h1> */}
+        <div>
+          <img className="absolute top-0 left-0 w-full h-screen" src="./assets/hero-bg.png" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Home
