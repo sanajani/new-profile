09d69742cd63514841516796f1659e51453d33bb
Sanaullah <webdevfarsi@gmail.com>
2023-06-08 16:23:31 +0430

    header section design

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,10 +1,28 @@
-
+import { NavLink } from 'react-router-dom'
+// import { useState } from 'react'
 
 
 const Navbar = () => {
+
   return (
-    <div className="w-screen py-5 bg-pink-500">
-        <h1>Navbar</h1>
+    <div className="bg-transparent fixed top-0 left-0 w-screen z-10">
+      <nav className="max-w-[1200px] py-3 mx-auto">
+        <ul className="flex justify-center items-center gap-10">
+          {/* <li className={`text-white md:text-2xl text-1xl`}><NavLink to='/'>Home</NavLink></li> */}
+          <NavLink
+            to="tasks"
+            className={({ isActive }) =>
+              isActive ? 'bg-red-500' : undefined
+            }
+          >
+            Tasks
+          </NavLink>
+          {/* className={`${active?'font-bold text-red':'text-gray-900'} */}
+          <li className={`text-white md:text-2xl text-1xl`}><NavLink to='/about'>About</NavLink></li>
+          <li className={`text-white md:text-2xl text-1xl`}><NavLink to='/project'>Project</NavLink></li>
+          <li className={`text-white md:text-2xl text-1xl`}><NavLink to='/contact'>Contact</NavLink></li>
+        </ul>
+      </nav>
     </div>
   )
 }
